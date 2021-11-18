7fac5f03f39f8289a8975b8cb9fba366e7f49b05
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-11-18 10:02:38 +0200

    feat: added knowledge to nodejs

--- code.js
+++ code.js
@@ -1,53 +1,48 @@
-import { OfflineState } from "./offlineState.js";
-import { OnlineState } from "./onlineState.js";
-import jsonOverTcp from "json-over-tcp-2"; // (1)
-export class OfflineState {
-  constructor(failsafeSocket) {
-    this.failsafeSocket = failsafeSocket;
-  }
-  send(data) {
-    // (2)
-    this.failsafeSocket.queue.push(data);
-  }
-  activate() {
-    // (3)
-    const retry = () => {
-      setTimeout(() => this.activate(), 1000);
-    };
-    console.log("Trying to connect...");
-    this.failsafeSocket.socket = jsonOverTcp.connect(
-      this.failsafeSocket.options,
-      () => {
-        console.log("Connection established");
-        this.failsafeSocket.socket.removeListener("error", retry);
-        this.failsafeSocket.changeState("online");
+const inputText = "something is something but something is nothing pls check nothing";
+const lineSplitter = (text, textSize) => {
+  const resultSplitText = [];
+  let counter = 0;
+  const textSplitArray = text.split(" ");
+  for (let i = 0; i < textSplitArray.length; i++) {
+    if (!resultSplitText[counter]) {
+      if (textSplitArray[i].length > textSize) {
+        resultSplitText.push(textSplitArray[i].substring(0, textSize));
+        resultSplitText.push(textSplitArray[i].substring(textSize));
+        counter += 2;
+      } else {
+        resultSplitText.push(textSplitArray[i]);
       }
-    );
-    this.failsafeSocket.socket.once("error", retry);
+    } else if (
+      textSplitArray[i].length > textSize &&
+      resultSplitText[counter].length < textSize
+    ) {
+      resultSplitText[counter] = resultSplitText[counter] += ` ${textSplitArray[
+        i
+      ].substring(0, textSize - resultSplitText[counter].length)}`;
+      resultSplitText.push(
+        textSplitArray[i].substring(textSize - resultSplitText[counter].length)
+      );
+      counter += 1;
+    } else if (
+      textSplitArray[i].length > textSize &&
+      resultSplitText[counter].length === textSize
+    ) {
+      resultSplitText.push(textSplitArray[i].substring(0, textSize));
+      resultSplitText.push(textSplitArray[i].substring(textSize));
+      counter += 2;
+    } else if (
+      // one extra character for the space
+      resultSplitText[counter].length + textSplitArray[i].length + 1 <=
+      textSize
+    ) {
+      resultSplitText[counter] = resultSplitText[
+        counter
+      ] += ` ${textSplitArray[i]}`;
+    } else {
+      counter += 1;
+      resultSplitText.push(textSplitArray[i]);
+    }
   }
-}
-
-export class FailsafeSocket {
-  constructor(options) {
-    // (1)
-    this.options = options;
-    this.queue = [];
-    this.currentState = null;
-    this.socket = null;
-    this.states = {
-      offline: new OfflineState(this),
-      online: new OnlineState(this),
-    };
-    this.changeState("offline");
-  }
-  changeState(state) {
-    // (2)
-    console.log(`Activating state: ${state}`);
-    this.currentState = this.states[state];
-    this.currentState.activate();
-  }
-  send(data) {
-    // (3)
-    this.currentState.send(data);
-  }
-}
+  return resultSplitText;
+};
+console.log(lineSplitter(inputText, 8));
