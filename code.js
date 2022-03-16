63878c26a6ebf39d947f0198d2c7bb3114e09bbf
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2022-03-16 09:54:51 +0200

    feat: added knowledge

--- code.js
+++ code.js
@@ -1,14 +1,13 @@
-const soap = require("strong-soap").soap;
-// wsdl of the web service this client is going to invoke. For local wsdl you can use, url = './wsdls/stockquote.wsdl'
-const url = "https://10.2.1.108/tranwall/service/IssuerApi/v2.0?wsdl";
+function foo() {
+  var x = 1;
+  function bar() {
+    var y = 2;
+    console.log(x); // 1 (function `bar` closes over `x`)
+    console.log(y); // 2 (`y` is in scope)
+  }
+  bar();
+  console.log(x); // 1 (`x` is in scope)
+  console.log(y); // ReferenceError in strict mode, `y` is scoped to `bar`
+}
 
-soap.createClient(url, function (err, client) {
-  console.log(client);
-  client.setSecurity({
-    addOptions: function (options) {
-      options.rejectUnauthorized = false;
-      options.strictSSL = false;
-      options.agent = new https.Agent(options);
-    },
-  });
-});
+foo();
