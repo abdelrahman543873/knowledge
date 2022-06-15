1cf8d6324792fa2882c4a5382a9fb31ed8d07f9c
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2022-06-15 11:14:34 +0200

    feat: added owasp knowledge

--- code.js
+++ code.js
@@ -1,22 +1,26 @@
-<<<<<<< HEAD
-function foo() {
-  var x = 1;
-  function bar() {
-    var y = 2;
-    console.log(x); // 1 (function `bar` closes over `x`)
-    console.log(y); // 2 (`y` is in scope)
+function biggerIsGreater(w) {
+  var vals = w.split("");
+  var largestI = -1;
+  for (let i = 0; i < vals.length - 1; i++) {
+    if (vals[i] < vals[i + 1]) {
+      largestI = i;
+    }
   }
-  bar();
-  console.log(x); // 1 (`x` is in scope)
-  console.log(y); // ReferenceError in strict mode, `y` is scoped to `bar`
+  if (largestI === -1) {
+    return "no answer";
+  }
+  var largestJ = -1;
+  for (let j = 0; j < vals.length; j++) {
+    if (vals[largestI] < vals[j]) {
+      largestJ = j;
+    }
+  }
+  let temp = vals[largestI];
+  vals[largestI] = vals[largestJ];
+  vals[largestJ] = temp;
+  var endArray = vals.splice(largestI + 1);
+  endArray.reverse();
+  vals = vals.concat(endArray);
+  return vals.join("");
 }
-=======
-const someObject = { hello: "hello", sean: "sean" };
-Object.keys(someObject).forEach((property) => {
-  console.log(property);
-const soap = require("strong-soap").soap;
-// wsdl of the web service this client is going to invoke. For local wsdl you can use, url = './wsdls/stockquote.wsdl'
-const url = "https://10.2.1.108/tranwall/service/IssuerApi/v2.0?wsdl";
->>>>>>> 30c065de98128c918f564c270614c79e13b8cfdc
-
-foo();
+biggerIsGreater("hkdc");
